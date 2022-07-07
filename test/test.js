3fc9dba3f523bd62cfb9145ebb655d3271a2f81d
yulianim <yuliani_m@hotmail.com>
2022-07-07 14:37:10 -0500

    Adding mocha, chai and supertest libraries and writing the first test in the project

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const chai = require('chai');
+const request = require('supertest');
+const app = express();
+describe('POST Create requirement', () => {
+    it('should create wallet for the user', () => {
+        // code for testing the api
+});
+});
